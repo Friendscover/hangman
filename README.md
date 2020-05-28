a4ad0bab39db36179dd04ecc746d1bf432b60734
Friendscover <tobiaskloepper1@web.de>
2020-05-28 11:10:27 +0200

    Refactored some code and added a save/load functionality

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Hangamn
+# Hangman
 This is my take on TheOdinProjects "Hangman" project!
 * [TheOdinProject](https://www.theodinproject.com/courses/ruby-programming/lessons/file-i-o-and-serialization)
 
-## Getting Started
+## Overview
 This project gets a random word out of the 5desk.txt file found at 
 * [Scrapmaker.com](https://www.scrapmaker.com/view/twelve-dicts/5desk.txt)
 
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,26 +1,18 @@
-#class Hangman (load file, select word, current word + guessed word)
-#game class (hangman class, play_turns, get player input)
-#save/load file
+require "json"
+#rewrite attribute accessor to enable json
 
-#refactor to attr accessor 
 class Hangman
-  def initialize
-    @current_word = ""
-    @guess_word = ""
+  attr_accessor :current_word, :guess_word
+
+  def initialize(current_word, guess_word)
+    @current_word = current_word
+    @guess_word = guess_word
 
     until @current_word.length >= 5 && @current_word.length <= 11
-      set_current_word
+      set_current_word(generate_word)
     end
 
     set_guess_word
-  end
-
-  def get_current_word
-    @current_word
-  end
-
-  def get_guess_word
-    @guess_word
   end
 
   #if given char is not included current word => if wont execute (nil)
@@ -35,60 +27,98 @@ class Hangman
   end
 
   def compare_current_guess_word
-    return true if get_current_word == get_guess_word
+    return true if current_word == guess_word
+  end
+
+  #needs to be private
+  def set_current_word(word)
+    @current_word = word
   end
 
   private
 
-  def set_current_word
+  def generate_word
     text = File.open("5desk.txt", "r")
     random_number = Random.new
     random_number = random_number.rand(61406)
-    word = text.readlines[random_number].chomp
-    @current_word = word
+    word = text.readlines[random_number].chomp.downcase
   end
 
   def set_guess_word
-    current_word = get_current_word
-    @guess_word = current_word.gsub(/[A-Za-z]/, "-")
+    guess_word = current_word
+    @guess_word = guess_word.gsub(/[A-Za-z]/, "-")
   end
 end
 
+
 class Game
+  attr_accessor :guesses, :characters_already_chosen
+
   def initialize
-    @h1 = Hangman.new
-    @characters_already_chosen = []
-
-    set_guesses
-
-    play_a_game
-  end
-
-  def get_guesses
-    @guesses
+    if(File.exist?("save.json"))
+      @h1 = Hangman.new("", "")
+      load_game
+      play_a_game
+    else
+      @h1 = Hangman.new("", "")
+      @characters_already_chosen = []
+      set_guesses
+      play_a_game
+    end
   end
 
   def play_a_game
     puts "The word was choosen!"
 
-    until @h1.compare_current_guess_word || (get_guesses < 0)
+    until @h1.compare_current_guess_word || (guesses <= 0)
+      puts "You have #{guesses} guesses left!"
+
       play_a_turn
       
       decrease_guesses
+
+      save_game
     end
 
-    puts "The answer was #{@h1.get_current_word}!"
+    puts "The answer was #{@h1.current_word}!"
+
+    remove_save_data
   end
 
   def play_a_turn
-    puts "Enter your char to take a guess! The word: #{@h1.get_guess_word}"
-    players_choice = gets.chomp
+    puts "Enter your char to take a guess! The word: #{@h1.guess_word}"
+    players_choice = gets.chomp.downcase
 
     p set_character_already_chosen(players_choice)
 
     @h1.guess_character(players_choice)
+  end
 
-    puts "You have #{get_guesses} guesses left!"
+  def save_game
+    save = File.open("save.json", "w")
+
+    save.puts JSON.dump ({
+      :current_word => @h1.current_word,
+      :guess_word => @h1.guess_word,
+      :guesses => @guesses,
+      :characters_already_chosen => @characters_already_chosen
+    })
+
+    save.close
+  end
+
+  #currently the current instance of h1 is overwritten instead of initializing
+  #a new object out of the hangman class
+  def load_game
+    save = File.open("save.json", "r")
+    
+    data = JSON.load(save.read)
+    @h1.current_word = data["current_word"]
+    @h1.guess_word = data["guess_word"]
+    @guesses = data["guesses"]
+    @characters_already_chosen = data["characters_already_chosen"]
+  
+    save.close
   end
 
   private
@@ -98,11 +128,15 @@ class Game
   end
 
   def set_guesses
-    @guesses = @h1.get_current_word.length
+    @guesses = @h1.current_word.length
   end
 
   def set_character_already_chosen(character)
     @characters_already_chosen << character
+  end
+
+  def remove_save_data
+    File.delete("save.json")
   end
 
 end
